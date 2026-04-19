7fe64a111b31cc48759b5544f307ea16387b3779
MaXwel25 <VMVMaxim@mail.ru>
2026-04-19 17:28:54 +0300

    add ksp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
 }
 
 android {
@@ -15,6 +16,15 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        // для Room
+        javaCompileOptions {
+            annotationProcessorOptions {
+                arguments += mapOf(
+                    "room.schemaLocation" to "$projectDir/schemas",
+                    "room.incremental" to "true"
+                )
+            }
+        }
     }
     buildFeatures{
         viewBinding=true
@@ -46,6 +56,23 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation("androidx.preference:preference-ktx:1.2.1")
+
+// implementation(libs.androidx.lifecycle.viewmodel.ktx)   // закомментировать
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.1")
+
+// implementation(libs.kotlinx.coroutines.android)        // закомментировать
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
+
+// implementation(libs.androidx.room.runtime)             // закомментировать
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    //ksp("androidx.room:room-compiler:2.6.1")
+
+// остальные строки оставить как есть
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation("com.google.code.gson:gson:2.10.1")
 
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
